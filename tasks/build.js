53b55a82255ac01f8f952b120c896415bde5f5fb
Dayton Nolan <daytonn@gmail.com>
2014-08-15 10:24:56 -0500

    Add minified version to distribution

--- tasks/build.js
+++ tasks/build.js
@@ -1,9 +1,21 @@
 var gulp = require("gulp");
 var browserify = require("gulp-browserify");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
 
-gulp.task("build", function() {
-  gulp.src("lib/jskit.js")
+gulp.task('compile', function() {
+  return gulp.src("lib/jskit.js")
     .pipe(browserify({ insertGlobals: true }))
     .pipe(gulp.dest("dist")
     .pipe(gulp.dest("example")));
 });
+
+gulp.task('minify', ['compile'], function() {
+  return gulp.src("dist/jskit.js")
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("build", ["minify"]);
